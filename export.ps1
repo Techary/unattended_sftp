60c3c831efafa8cacbb3a6604b09475e19bacd3b
James Tarran <jtarran@techary.com>
2024-09-03 13:32:14 +0100

    Update export.ps1
    - Don't set attributes

--- export.ps1
+++ export.ps1
@@ -9,13 +9,17 @@ foreach ($path in $env:local_export_path) {
         $files = Get-ChildItem -Path $path -File
         # Iterate through each file in the directory
         foreach ($file in $files) {
+            # Construct the correct remote file path
             $remoteFilePath = Join-Path $env:remote_export_path $file.Name
+            # Set transfer options to avoid setting permissions or timestamps
             $transferOptions = New-Object WinSCP.TransferOptions
             $transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
+            $transferOptions.PreserveTimestamp = $false
+            $transferOptions.FilePermissions = $null  # Do not attempt to set permissions
             write-verbose "Uploading $($file.Name)"
             # Upload the file to the remote directory
             $transferResult = $Session.PutFiles($file.FullName, $remoteFilePath, $False, $transferOptions)
-            $transferResult.Check()
+            $transferResult.Check()  # Check for errors
             if (!$transferResult.error) {
                 Write-Host "Uploaded '$($file.Name)' to '$remoteFilePath'"
             } else {
